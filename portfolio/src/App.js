582a1a1ad6d44ee7107914eef1610411eda5f6b1
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-10-19 03:21:24 +0200

    4projects done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,47 @@
 import "./App.css";
-import tree from "./images/tree.jpg";
+import photo from "./images/photo.jpg";
 import arrowDown from "./icons/arrow-down.svg";
 import circles from "./images/circles.jpg";
 import github from "./icons/github.svg";
 import circlesBg from "./images/circlesBackground.png";
 import phonebook from "./images/phonebook.jpg";
 import phonebookBg from "./images/phonebookBg.jpg";
+import moviesearchBg from "./images/moviesearchBg.jpg";
+import moviesearch from "./images/moviesearch.jpg";
+import contactbookBg from "./images/contactbookBg.jpg";
+import linkedinLight from "./icons/linkedinLight.svg";
+import githubLight from "./icons/githubLight.svg";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={tree} className="App-logo" alt="logo" />
+				<div className="lines">
+					<div className="line"></div>
+					<div className="line"></div>
+					<div className="line"></div>
+				</div>
+				<div className="logos">
+					<img src={photo} className="App-logo" alt="logo" />
+					<div className="social-media">
+						<a
+							href="https://github.com/BartlomiejPop"
+							target="_blank"
+							rel="noreferrer">
+							<img src={githubLight} className="social-media-logo" alt="logo" />
+						</a>
+						<a
+							href="https://www.linkedin.com/in/bart%C5%82omiej-poprawa-961397247/"
+							target="_blank"
+							rel="noreferrer">
+							<img
+								src={linkedinLight}
+								className="social-media-logo"
+								alt="logo"
+							/>
+						</a>
+					</div>
+				</div>
 				<span className="name">Bartłomiej Poprawa</span>
 				<p className="header-description">
 					With strong university background and ability to progress I am willing
@@ -27,19 +57,20 @@ function App() {
 					</div>
 				</div>
 			</header>
-			<projects>
+			<div>
 				<div className="project-wrapper">
 					<h2 className="project-name" style={{ color: "#d1caca" }}>
 						CIRCLES
 					</h2>
 					<div className="project-container">
-						<img src={circlesBg} className="circlesBg" />
+						<img alt="bg" src={circlesBg} className="circlesBg" />
 						<div className="circles-box">
 							<div className="circles-link">
 								<a
 									href="https://bartlomiejpop.github.io/circles-ranking/"
-									target="_blank">
-									<img className="project-image" src={circles} />
+									target="_blank"
+									rel="noreferrer">
+									<img alt="project" className="project-image" src={circles} />
 								</a>
 							</div>
 							<div className="circles-description">
@@ -52,8 +83,9 @@ function App() {
 								<div className="circles-gitHub">
 									<a
 										href="https://github.com/BartlomiejPop/circles-ranking"
-										target="_blank">
-										<img className="github-icon" src={github} />
+										target="_blank"
+										rel="noreferrer">
+										<img alt="ghIcon" className="github-icon" src={github} />
 									</a>
 								</div>
 							</div>
@@ -72,13 +104,18 @@ function App() {
 						PHONEBOOK
 					</h2>
 					<div className="project-container">
-						<img src={phonebookBg} className="circlesBg" />
+						<img alt="bg" src={phonebookBg} className="circlesBg" />
 						<div className="phonebook-box">
 							<div className="circles-link">
 								<a
 									href="https://bartlomiejpop.github.io/goit-react-hw-08-phonebook/"
-									target="_blank">
-									<img className="project-image" src={phonebook} />
+									target="_blank"
+									rel="noreferrer">
+									<img
+										alt="phonebookImage"
+										className="project-image"
+										src={phonebook}
+									/>
 								</a>
 							</div>
 							<div className="circles-description">
@@ -99,8 +136,9 @@ function App() {
 									style={{ backgroundColor: " #e6dfdf" }}>
 									<a
 										href="https://github.com/BartlomiejPop/goit-react-hw-08-phonebook"
-										target="_blank">
-										<img className="github-icon" src={github} />
+										target="_blank"
+										rel="noreferrer">
+										<img alt="ghIcon" className="github-icon" src={github} />
 									</a>
 								</div>
 							</div>
@@ -115,37 +153,118 @@ function App() {
 					</div>
 				</div>
 				<div className="project-wrapper">
-					<h2 className="project-name">CIRCLES</h2>
+					<h2 className="project-name" style={{ color: "rgb(227, 100, 50,1)" }}>
+						MOVIESEARCH
+					</h2>
 					<div className="project-container">
-						<div className="circles-box">
-							<div className="circles-link"></div>
+						<img
+							alt="movieSearchBg"
+							src={moviesearchBg}
+							className="circlesBg"
+						/>
+						<div className="moviesearch-box">
+							<div className="circles-link">
+								<a
+									href="https://michalmazurdev.github.io/team-project-filmoteka/index.html"
+									target="_blank"
+									rel="noreferrer">
+									<img
+										alt="moviesearchImage"
+										className="project-image"
+										src={moviesearch}
+									/>
+								</a>
+							</div>
 							<div className="circles-description">
-								<div>DESCRIPTION</div>
-								<div className="circles-gitHub"></div>
+								<p
+									className="project-description"
+									style={{
+										color: "#fff",
+										backgroundColor: " rgb(227, 100, 50,0.7)",
+										fontSize: "14px",
+									}}>
+									Application depended on choosing a movie created by the group
+									of programmers in which I took role as a programmer. App
+									component with Public and Private routes, AuthPage page logic.
+								</p>
+								<div
+									className="circles-gitHub"
+									style={{ backgroundColor: " rgb(227, 100, 50,0.7)" }}>
+									<a
+										href="https://github.com/BartlomiejPop/team-project-filmoteka"
+										target="_blank"
+										rel="noreferrer">
+										<img alt="ghIcon" className="github-icon" src={github} />
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
 					<div className="header-sign">
 						<div className="header-sign-wrapper">
 							<img alt="arrow icon" style={{ width: "30px" }} src={arrowDown} />
-							<span>NODE DATABASE </span>
+							<span>MONGODB CONTACTBOOK </span>
 							<img alt="arrow icon" style={{ width: "30px" }} src={arrowDown} />
 						</div>
 					</div>
 				</div>
 				<div className="project-wrapper">
-					<h2 className="project-name">NODE DATABASE</h2>
+					<h2 className="project-name" style={{ color: "rgb(106, 170, 91)" }}>
+						MONGODB CONTACTBOOK
+					</h2>
 					<div className="project-container">
-						<div className="circles-box">
-							<div className="circles-link"></div>
+						<img
+							alt="contactbookBg"
+							src={contactbookBg}
+							className="circlesBg"
+						/>
+						<div className="contactbook-box">
+							<div className="circles-link">
+								<a
+									href="https://github.com/BartlomiejPop/REST-API-application"
+									target="_blank"
+									rel="noreferrer">
+									<img
+										alt="contactbookBg"
+										className="project-image"
+										src={contactbookBg}
+									/>
+								</a>
+							</div>
 							<div className="circles-description">
-								<div>DESCRIPTION</div>
-								<div className="circles-gitHub"></div>
+								<p
+									className="project-description"
+									style={{
+										color: "#5f5e5e",
+										backgroundColor: "rgb(153, 212, 138,0.4)",
+										fontSize: "15px",
+									}}>
+									Rest API application developed with Express.js and MongoDB
+									database. Authentication system. Endpoints described to allow
+									managing actions on particular contacts
+								</p>
+								<div
+									className="circles-gitHub"
+									style={{ backgroundColor: "rgb(153, 212, 138,0.4)" }}>
+									<a
+										href="https://github.com/BartlomiejPop/REST-API-application"
+										target="_blank"
+										rel="noreferrer">
+										<img alt="ghIcon" className="github-icon" src={github} />
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
+					<div className="header-sign">
+						<div className="header-sign-wrapper">
+							<img alt="arrow icon" style={{ width: "30px" }} src={arrowDown} />
+							<span>MOVIESEARCH </span>
+							<img alt="arrow icon" style={{ width: "30px" }} src={arrowDown} />
+						</div>
+					</div>
 				</div>
-			</projects>
+			</div>
 		</div>
 	);
 }
